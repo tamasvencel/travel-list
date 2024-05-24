9620e0b1733b4165134466843d06ed4f6151aea7
tamasvencel <tamasvenci46@gmail.com>
2024-05-24 09:05:01 +0300

    travel-list

---
+++ src/components/Stats.js
@@ -0,0 +1,23 @@
+export default function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const numPackedPercentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats" id="clouds">
+      <em>
+        {numPackedPercentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `💼You have ${numItems} items on your list, and you already packed ${numPacked} (${numPackedPercentage}%)`}
+      </em>
+    </footer>
+  );
+}
